06bec5e3e2ec30e2fa767979aa2ddff0563cc8f5
Gregory Maynard <cinderscript@gmail.com>
2018-08-30 15:53:57 -0700

    GUISystem and GUIScreen and unity update

---
+++ Assets/Scripts/Mono/GUI/GUIScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace IronGrimoire.GuiBase
+{
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(CanvasGroup))]
+	public class GUIScreen : MonoBehaviour
+	{
+		[Header( "Main Properties" )]
+		public Selectable SelectOnStartup;
+
+		[Header( "Events" )]
+		public UnityEvent OnClosed = new UnityEvent();
+		public UnityEvent OnOpened = new UnityEvent();
+
+		private Animator animator;
+
+		void Start()
+		{
+			animator = GetComponent<Animator>();
+
+			if (SelectOnStartup)
+			{
+				EventSystem.current.SetSelectedGameObject( SelectOnStartup.gameObject );
+			}
+		}
+
+		public void Close()
+		{
+			Debug.Log( "closed: " + gameObject );
+			OnClosed?.Invoke();
+			animator.SetTrigger( "hide" );
+		}
+
+		public void Open()
+		{
+			Debug.Log( "opened: "+gameObject );
+
+			OnOpened?.Invoke();
+			animator.SetTrigger( "show" );
+		}
+	}
+}
---
+++ Assets/Scripts/Mono/GUI/GUISystem.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace IronGrimoire.GuiBase
+{
+	public class GUISystem : MonoBehaviour
+	{
+		[Header( "GUI System Properties" )]
+		public GUIScreen StartScreen;
+
+		[Header( "GUI System Events" )]
+		public UnityEvent OnSwitchedScreens = new UnityEvent();
+
+		[Header( "Screen Transition Fader" )]
+		public Image Fader;
+		public float FadeDuration = 1;
+
+		public GUIScreen PreviousScreen { get; private set; }
+		public GUIScreen CurrentScreen { get; private set; }
+
+		private GUIScreen[] screens = new GUIScreen[0];
+
+		private void Start()
+		{
+			screens = GetComponentsInChildren<GUIScreen>(true);
+
+			SwitchScreens( StartScreen );
+
+			if (Fader)
+			{
+				Fader.gameObject.SetActive( true );
+			}
+			FadeIn();
+		}
+
+		public void SwitchScreens(GUIScreen newScreen)
+		{
+			if (newScreen)
+			{
+				if (CurrentScreen)
+				{
+					CurrentScreen.Close();
+					PreviousScreen = CurrentScreen;
+				}
+
+				CurrentScreen = newScreen;
+				CurrentScreen.gameObject.SetActive( true );
+				CurrentScreen.Open();
+
+				OnSwitchedScreens?.Invoke();
+			}
+		}
+
+		public void FadeIn()
+		{
+			Fader?.CrossFadeAlpha( 0, FadeDuration / 2, true );
+		}
+		public void FadeOut()
+		{
+			Fader?.CrossFadeAlpha( 1, FadeDuration / 2, true );
+
+		}
+
+		public void GoToPrevious(GUIScreen screen)
+		{
+			if (PreviousScreen)
+			{
+				SwitchScreens( PreviousScreen );
+			}
+		}
+	}
+}
