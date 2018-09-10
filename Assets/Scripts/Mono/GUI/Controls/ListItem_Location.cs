cb7b32d328d734b63e997392d0085f4e2d7c509d
Gregory Maynard <cinderscript@gmail.com>
2018-09-10 16:16:03 -0700

    Location Selection scrollview

---
+++ Assets/Scripts/Mono/GUI/Controls/ListItem_Location.cs
@@ -0,0 +1,24 @@
+﻿using AIRogue.GameObjects;
+
+using UnityEngine.UI;
+
+namespace IronGrimoire.Gui.Game
+{
+	class ListItem_Location : SelectableListItem
+	{
+		public Image Icon = null;
+		public Text Location = null;
+		public Text Enemies = null;
+		public Text Winnings = null;
+
+		public void Initialize(Weapon weapon)
+		{
+			//Icon.sprite = weapon.Icon;
+			//Name.text = weapon.WeaponName.ToString();
+			//Type.text = weapon.WeaponType_GUI;
+			//Value.text = weapon.Value.ToString();
+
+			//TaggedObject = weapon;
+		}
+	}
+}
