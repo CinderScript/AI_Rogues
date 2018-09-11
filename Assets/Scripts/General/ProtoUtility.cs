b77e6e12d167d1bc99a468536c721f187bb358df
Gregory Maynard <cinderscript@gmail.com>
2018-09-11 16:37:24 -0700

    protoutility

---
+++ Assets/Scripts/General/ProtoUtility.cs
@@ -0,0 +1,53 @@
+﻿using ProtoBuf;
+using System;
+using System.IO;
+
+namespace Utility
+{
+	class ProtoUtility
+	{
+		public static Exception SaveToFile<Object>(string filePath, Object serializable)
+		{
+			Exception ex = null;
+
+			try
+			{
+				FileInfo fileInfo = new FileInfo( filePath );
+				Directory.CreateDirectory( fileInfo.Directory.FullName );
+
+				using (FileStream stream = File.Open( filePath, FileMode.Create ))
+				{
+					Serializer.Serialize( stream, serializable );
+				}
+			}
+			catch (Exception e)
+			{
+				ex = e;
+			}
+
+			return ex;
+		}
+		public static Exception LoadFromFile<Object>(string filePath, out Object deserializable)
+		{
+			Exception ex = null;
+
+			try
+			{
+				FileInfo fileInfo = new FileInfo( filePath );
+				Directory.CreateDirectory( fileInfo.Directory.FullName );
+
+				using (FileStream stream = File.Open( filePath, FileMode.OpenOrCreate ))
+				{
+					deserializable = Serializer.Deserialize<Object>( stream );
+				}
+			}
+			catch (Exception e)
+			{
+				deserializable = default( Object );
+				ex = e;
+			}
+
+			return ex;
+		}
+	}
+}
