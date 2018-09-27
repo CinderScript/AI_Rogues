40cf85b1acb6f9f07fad398f95d9ef41129ba5ae
Gregory Maynard <cinderscript@gmail.com>
2018-09-27 14:07:43 -0700

    Created WeaponSlot_ListItem

---
+++ Assets/Scripts/Mono/GUI/Controls/ListItem_WeaponSlot.cs
@@ -0,0 +1,16 @@
+﻿using AIRogue.GameObjects;
+
+using UnityEngine.UI;
+
+namespace IronGrimoire.Gui.Game
+{
+	class ListItem_WeaponSlot : SelectableListItem
+	{
+		public Text Name = null;
+
+		public void Initialize(Weapon weapon)
+		{
+			Name.text = weapon.WeaponName.ToString();
+		}
+	}
+}
